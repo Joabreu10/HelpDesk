790b50638627eb39b7cdf59196d74b46a2ab735f
Jonas Abreu <jvdsa1893@gmail.com>
2023-11-15 11:44:25 -0300

    anotacoes JPA para classes de domínio

--- src/main/java/com/record/helpdesk/domain/Tecnico.java
+++ src/main/java/com/record/helpdesk/domain/Tecnico.java
@@ -3,18 +3,26 @@ package com.record.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.record.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
-		// TODO Auto-generated constructor stub
+		addPerfis(Perfil.TECNICO);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
-		// TODO Auto-generated constructor stub
+		addPerfis(Perfil.TECNICO);
 	}
 
 	public List<Chamado> getChamados() {

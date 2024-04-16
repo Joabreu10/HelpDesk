ce36439ce32179cfc53825e36a008bdc678c5fe3
Jonas Abreu <jvdsa1893@gmail.com>
2024-04-16 18:35:20 -0300

    Create para chamado

--- src/main/java/com/record/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/record/helpdesk/resources/ChamadoResource.java
@@ -6,8 +6,10 @@ import com.record.helpdesk.services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,6 +32,14 @@ public class ChamadoResource {
         List<Chamado> list = chamadoService.findAll();
         List<ChamadoDTO> listDTO = list.stream().map( obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity <ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+        Chamado obj = chamadoService.create(objDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().
+                path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/record/helpdesk/services/ChamadoService.java
+++ src/main/java/com/record/helpdesk/services/ChamadoService.java
@@ -1,7 +1,11 @@
 package com.record.helpdesk.services;
 
 import com.record.helpdesk.domain.Chamado;
+import com.record.helpdesk.domain.Cliente;
+import com.record.helpdesk.domain.Tecnico;
 import com.record.helpdesk.domain.dtos.ChamadoDTO;
+import com.record.helpdesk.domain.enums.Prioridade;
+import com.record.helpdesk.domain.enums.Status;
 import com.record.helpdesk.repositories.ChamadoRepository;
 import com.record.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,10 @@ public class ChamadoService {
 
     @Autowired
     ChamadoRepository chamadoRepository;
+    @Autowired
+    TecnicoService tecnicoService;
+    @Autowired
+    ClienteService clienteService;
 
     public Chamado findById(Integer id){
         Optional <Chamado> obj = chamadoRepository.findById(id);
@@ -26,4 +34,25 @@ public class ChamadoService {
         return chamadoRepository.findAll();
     }
 
+    public Chamado create(ChamadoDTO objDTO) {
+        return chamadoRepository.save(newChamado(objDTO));
+    }
+
+    private Chamado newChamado(ChamadoDTO obj){
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null){
+            chamado.setId(obj.getId());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
+    }
 }
